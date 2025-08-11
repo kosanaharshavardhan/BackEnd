3a1801f698f98915ea227e3ffb74879fd1648c45
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-08-12 00:04:48 +0530

    donr

--- Nodejs/firstserver/app.js
+++ Nodejs/firstserver/app.js
@@ -3,4 +3,6 @@ function requestcall(req,res){
     console.log(req);
 }
 const server=http.createServer(requestcall);
-server.listen(3001);
+server.listen(3001,()=>{
+    console.log("fetching at 3001")
+});
