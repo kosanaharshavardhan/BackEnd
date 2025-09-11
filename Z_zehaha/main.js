53f0123dcf2ef5bbbaad3648c1321e11887db659
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-09-12 00:11:15 +0530

    ejs set up done

--- Z_zehaha/main.js
+++ Z_zehaha/main.js
@@ -6,6 +6,8 @@ const app=express();
 const rootdir=require('./utils/pathutil');
 const hostroute=require('./routes/hostroute');
 const useroute=require('./routes/userroute');
+app.set("view engine","ejs");
+app.set("views","views");
 
 app.use(express.urlencoded());
 app.use("/host",hostroute);
@@ -15,7 +17,9 @@ app.use(useroute);
 app.use(express.static(path.join(rootdir,"public")));
 app.use((req,res,next)=>{
     // res.status(404).send("<h1>404 Not found</h1>")
-    res.status(404).sendFile(path.join(rootdir,"views","404.html"));
+    // res.status(404).sendFile(path.join(rootdir,"views","404.html"));
+    let housename="WELL WELL WELL";
+    res.status(404).render('404',{housename});
 })
 
 const PORT=3001;
