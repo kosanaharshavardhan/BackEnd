7369689de01455ffc910f0f8d4a96b2d82cf9ef9
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-09-13 00:31:52 +0530

    done with partials <%-include('/a/s/')%>

--- Z_zehaha/main.js
+++ Z_zehaha/main.js
@@ -19,7 +19,7 @@ app.use((req,res,next)=>{
     // res.status(404).send("<h1>404 Not found</h1>")
     // res.status(404).sendFile(path.join(rootdir,"views","404.html"));
     let housename="WELL WELL WELL";
-    res.status(404).render('404',{housename});
+    res.status(404).render('404',{housename,title:'Page Not Found12309232310u3'});
 })
 
 const PORT=3001;
