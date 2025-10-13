d99e23da8a2a9d6ec5f8ded4ab6e694e8048bfcb
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-10-13 16:45:34 +0530

    some points

--- M_E_N/db/user-model.js
+++ M_E_N/db/user-model.js
@@ -6,5 +6,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mydb');
 const userscheme=mongoose.Schema({
     "name":String,
     "age":Number,
+    "post":[
+        {
+            type:mongoose.Schema.Types.ObjectId // type:"id"
+            // ref:'schemaname of that object whose ids are coming'
+            //user.save() for explicit changes
+        }
+    ]
+
 })
 module.exports=mongoose.model("user",userscheme);
