ade9f922f2152942504be170410988ed57e3952b
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-10-19 20:56:44 +0530

    boiler code

--- M_E_N/Try_pack/app.js
+++ M_E_N/Try_pack/app.js
@@ -1,2 +1,15 @@
 const express=require('express')
-const app=
+const app=express();
+
+const joke=require('give-me-a-joke');
+
+let jokee="";
+joke.getCustomJoke((joke)=>{
+    jokee=joke;
+})
+app.get('/',(req,res)=>{
+
+    res.send(jokee)
+})
+
+app.listen(5000)
