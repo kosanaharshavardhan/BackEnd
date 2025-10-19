f1ca8b418baa4246d75f7863f83f94cbe584b9ea
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-10-19 21:04:55 +0530

    figlet

--- M_E_N/Try_pack/app.js
+++ M_E_N/Try_pack/app.js
@@ -4,12 +4,12 @@ const app=express();
 const joke=require('give-me-a-joke');
 
 let jokee="";
-joke.getCustomJoke((joke)=>{
-    jokee=joke;
-})
-app.get('/',(req,res)=>{
 
-    res.send(jokee)
+app.get('/',(req,res)=>{
+joke.getCustomJoke(async (joke)=>{
+    console.log(joke)
+    await res.send(joke);
+})
 })
 
 app.listen(5000)
