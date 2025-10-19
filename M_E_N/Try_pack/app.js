b49dc8222e29f7e1ed063f78d0ec623c55db369a
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-10-19 21:09:26 +0530

    figlet

--- M_E_N/Try_pack/app.js
+++ M_E_N/Try_pack/app.js
@@ -1,15 +1,29 @@
 const express=require('express')
 const app=express();
 
-const joke=require('give-me-a-joke');
+// const joke=require('give-me-a-joke');
+const fig=require('figlet')
 
-let jokee="";
+// let jokee="";
 
+// app.get('/',(req,res)=>{
+// joke.getCustomJoke(async (joke)=>{
+//     console.log(joke)
+//     await res.send(joke);
+// })
+// })
+
+
+// import figlet from "figlet";
+let text;
+async function doStuff() {
+    text = await fig.text("Hello World!!");
+  console.log(text);
+}
+
+doStuff();
 app.get('/',(req,res)=>{
-joke.getCustomJoke(async (joke)=>{
-    console.log(joke)
-    await res.send(joke);
-})
+    // fig.res("Harsha") not there this func
 })
 
 app.listen(5000)
